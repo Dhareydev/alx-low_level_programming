b6646927f6b471a6432ef9108189f4a1858aa972
Bello Emmanuel <Dharexbee21@gmail.com>
2023-02-16 14:45:45 -0800

    a C program that prints followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -a C program that prints exactly a line, followed by a new line, to the standard error
+ * main -a C program that prints, followed by a new line, to the standard error
  * Return: 1
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
